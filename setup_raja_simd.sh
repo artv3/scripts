dc6508041b466a2ac84c01d7c77a95364c6cdddc
Arturo Vargas <artv3.89@gmail.com>
2018-03-13 23:12:46 -0700

    raja setup script

--- setup_raja_simd.sh
+++ setup_raja_simd.sh
@@ -1,18 +1,9 @@
 module load cmake 
-git clone -b simd1 https://github.com/artv3/RAJA.git RAJA_SIMD1 &&
-cd RAJA_SIMD1 && git submodule init && git submodule update && mkdir build-intel && cd build-intel
-cmake -DCMAKE_CXX_COMPILER=icpc -DCMAKE_C_COMPILER=icc -DCMAKE_INSTALL_PREFIX:PATH=$(pwd) ../
-cd ../../
 
-
-
-#git clone -b simd2 https://github.com/artv3/RAJA.git RAJA_SIMD2
-#git clone -b simd3 https://github.com/artv3/RAJA.git RAJA_SIMD3
-#git clone -b simd4 https://github.com/artv3/RAJA.git RAJA_SIMD4
-#git clone -b simd5 https://github.com/artv3/RAJA.git RAJA_SIMD5
-#git clone -b simd6 https://github.com/artv3/RAJA.git RAJA_SIMD6
-#git clone -b simd7 https://github.com/artv3/RAJA.git RAJA_SIMD7
-#git clone -b simd8 https://github.com/artv3/RAJA.git RAJA_SIMD8
-#git clone -b simd9 https://github.com/artv3/RAJA.git RAJA_SIMD9
-
-
+for N in {1..9}
+do
+  git clone -b simd${N} https://github.com/artv3/RAJA.git RAJA_SIMD${N}
+  cd RAJA_SIMD${N} && git submodule init && git submodule update && mkdir build-intel
+  cd build-intel && cmake -DCMAKE_CXX_COMPILER=icpc -DCMAKE_C_COMPILER=icc -DCMAKE_INSTALL_PREFIX:PATH=$(pwd) ../
+  make -j && make install && cd ../../
+done  
